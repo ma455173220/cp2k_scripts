fc760a0687a385bcfdaf717d54c304bb3a5be744
HONGYANG MA <hm1876@gadi-login-02.gadi.nci.org.au>
2024-04-09 14:19:40 +1000

    merge cp2k2pos and cp2k2cif

--- cp2k2cif.sh
+++ cp2k2cif.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
 
-if [ $1 ] ; then
-	input_file=$1
+if [ "$1" ]; then
+    input_file="$1"
 else
-	echo -e "\033[31mERROR:\033[0m Missing file operand! Please identify the name of geometry file!"
-	exit 1
+    echo -e "\033[31mERROR:\033[0m Missing file operand! Please identify the name of the geometry file!"
+    exit 1
 fi
 
-echo -e "Choose the format of the output file:\n1.pdb\n2.xyz\n3.cif"
-read output_format_choice
+echo -e "Choose the format of the output file:\n1. POSCAR \n2. cif \n3. pdb\n4. xyz"
+read -p "Enter your choice: " output_format_choice
 
-if [ $output_format_choice != 3 ] ; then
-	echo -e "$1\n100\n2\n$output_format_choice\n\n0\nq" | Multiwfn 1>>/dev/null
-else
-	echo -e "$1\n100\n2\n33\n\n0\nq" | Multiwfn 1>>/dev/null
-fi
+case $output_format_choice in
+    1)
+        output_format=27
+        ;;
+    2)
+        output_format=33
+        ;;
+    3)
+        output_format=1
+        ;;
+    4)
+        output_format=2
+        ;;
+    *)
+        echo -e "\033[31mERROR:\033[0m Invalid choice!"
+        exit 1
+        ;;
+esac
 
+echo -e "$input_file\n100\n2\n$output_format\n\n0\nq" | Multiwfn 1>>/dev/null
+
+echo "Done!"
